f268fd2fa34954fe356e098b0e9a67a42b14e555
Mario S. Valdés <mariosergiovaldes145@gmail.com>
2023-03-22 22:22:57 -0500

    namd implementation

--- xBFreE/mmpbsa/commandlineparser/__init__.py
+++ xBFreE/mmpbsa/commandlineparser/__init__.py
@@ -1,7 +1,6 @@
 from .gmx import gmx_parser
 from .amber import amber_parser
-
-# from .namd import namd_parser
+from .namd import namd_parser
 
 
 
--- xBFreE/mmpbsa/commandlineparser/namd.py
+++ xBFreE/mmpbsa/commandlineparser/namd.py
@@ -1,50 +1,69 @@
+# ##############################################################################
+#                            GPLv3 LICENSE INFO                                #
+#  Copyright (c) 2023.  Mario S. Valdes-Tresanco and Mario E. Valdes-Tresanco  #
+#                                                                              #
+#  This program is free software; you can redistribute it and/or modify it     #
+#  under the terms of the GNU General Public License version 3 as published    #
+#  by the Free Software Foundation.                                            #
+#                                                                              #
+#  This program is distributed in the hope that it will be useful, but         #
+#  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY  #
+#  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License    #
+#  for more details.                                                           #
+# ##############################################################################
+
+"""
+This module contains classes and such that are responsible for parsing
+command-line arguments for gmx_MMPBSA.  All the files specified for use
+in gmx_MMPBSA will be assigned as attributes to the returned class.
+
+"""
+
 from xBFreE.mmpbsa.commandlineparser.parsers import (mmpbsa_parser,
-                                                 namd_trajectory, namd_topology, pdb,
-                                                 complex_group_des, receptor_group_des, ligand_group_des,
-                                                 add_miscellaneous_actions)
+                                                     namd_topology, namd_trajectory, pdb, mask,
+                                                     complex_group_des, receptor_group_des, ligand_group_des,
+                                                     add_miscellaneous_actions)
+from xBFreE import __version__
 
 namd_parser = mmpbsa_parser()
-
+namd_parser.add_argument('-v', '--version', action='version',
+                          version='''%%(prog)s %s''' % __version__)
 group = namd_parser.add_argument_group('Complex', complex_group_des)
-group.add_argument('-cp', dest='complex_prmtop', metavar='<Topology File>', type=namd_topology, default=None,
+group.add_argument('-cs', dest='complex_structure', metavar='<Structure File>', default=None, type=pdb,
+                   help='''Structure file of the complex. Allowed formats: *.pdb''', required=True)
+group.add_argument('-cp', dest='complex_top', metavar='<Topology File>', type=namd_topology, default=None,
                    help='''The complex Topology file''')
-group.add_argument('-cg', dest='complex_groups', metavar='aa selection', nargs=2, default=None, type=str,
-                   help='Selection for receptor and ligand in complex topology file. Both Amber mask '
-                        'and residue selection are allowed. The notation is as follows: "-cg '
-                        '<Receptor group> <Ligand group>", ie. -cg :1-100 :101-120 (Amber mask) or A/1-100 B/101-120 '
-                        '(Residue selection)')
+group.add_argument('-cg', dest='complex_groups', metavar=('<receptor_mask>', '<ligand_mask>'), nargs=2, default=None,
+                   type=mask,
+                   help='Selection for receptor and ligand in complex topology file using namd mask. '
+                        'The notation is as follows: '
+                        '"-cg <receptor_mask> <ligand_mask>", ie. -cg :1-100,121-150 :101-120')
 group.add_argument('-ct', dest='complex_trajs', nargs='*', metavar='TRJ', type=namd_trajectory,
-                   help='''Complex trajectories. Make sure the trajectory is fitted and
-                         pbc have been removed. Allowed formats: *.dcd (recommended), *.pdb,
-                         (specify as many as you'd like).''')
-group.add_argument('-cr', dest='reference_structure', metavar='<PDB File>', default=None, type=pdb,
-                   help='''Complex Reference Structure file. This option is optional but recommended
-                         (Use the PDB file used to generate the topology in NAMD). If not defined,
-                         the chains ID assignment will be done automatically according to the
-                         structure (can generate wrong mapping).''')
+                   help='''Complex trajectories. Make sure the trajectory is fitted and pbc have been removed. 
+                   Allowed formats: *.mdcrd, *.crd, *.pdb, *.nc (specify as many as you'd like).''')
+
 group = namd_parser.add_argument_group('Receptor', receptor_group_des)
-group.add_argument('-rp', dest='receptor_prmtop', metavar='<Topology File>', type=namd_topology,
+group.add_argument('-rp', dest='receptor_top', metavar='<Topology File>', type=namd_topology,
                    help='''Topology file of the unbound receptor.''')
-group.add_argument('-rg', dest='receptor_group', metavar='index', default=None, type=str,
-                   help='''Receptor group in receptor topology file. Both Amber mask '
-                        'and residue selection are allowed. The notation is as follows: "-rg '
-                        '<Receptor group>", ie. -rg :1-100 (Amber mask) or A/1-100 (Residue selection)''')
+group.add_argument('-rg', dest='receptor_group', metavar='<receptor_mask>', default=None, type=mask,
+                   help='Receptor group in receptor topology file using namd mask. '
+                        'The notation is as follows: '
+                        '"-rg <receptor_mask>", ie. -rg :1-100')
 group.add_argument('-rt', dest='receptor_trajs', nargs='*', metavar='TRJ', type=namd_trajectory,
                    help='''Input trajectories of the unbound receptor for multiple trajectory approach.
-                         Allowed formats: *.dcd (recommended), *.pdb, *.nc (specify as many
+                         Allowed formats: *.mdcrd, *.crd, *.pdb, *.nc (specify as many
                          as you'd like).''')
+
 group = namd_parser.add_argument_group('Ligand', ligand_group_des)
-group.add_argument('-lp', dest='ligand_prmtop', metavar='<Topology File>', type=namd_topology,
+group.add_argument('-lp', dest='ligand_top', metavar='<Topology File>', type=namd_topology,
                    help='''Topology file of the unbound ligand.''')
-group.add_argument('-lg', dest='ligand_group', metavar='index', default=None, type=str,
-                   help='''Ligand group in ligand topology file. Both Amber mask '
-                        'and residue selection are allowed. The notation is as follows: "-lg '
-                        '<Ligand group>", ie. -lg :101-120 (Amber mask) or B/101-120 (Residue selection)''')
+group.add_argument('-lg', dest='ligand_group', metavar='<ligand_mask>', default=None, type=mask,
+                   help='Ligand group in ligand topology file using namd mask. '
+                        'The notation is as follows: '
+                        '"-lg <ligand_mask>", ie. -lg :1-20')
 group.add_argument('-lt', dest='ligand_trajs', nargs='*', metavar='TRJ', type=namd_trajectory,
                    help='''Input trajectories of the unbound ligand for multiple trajectory approach.
-                         Allowed formats: *.dcd (recommended), *.pdb, *.nc (specify as many
+                         Allowed formats: *.mdcrd, *.crd, *.pdb, *.nc (specify as many
                          as you'd like).''')
 
 add_miscellaneous_actions(namd_parser)
-
-# namd_parser.parse_args(['-h'])
